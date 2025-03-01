3ae67dd0b391b2e2898c57a8d91961e531546324
Nudesuppe42 <jufo.theo@gmail.com>
2025-03-01 16:04:01 +0100

    Add PWA support and static file handling with headers configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,31 @@
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   plugins: [
     VitePWA({
       registerType: "autoUpdate",
+      manifest: {
+        name: "!see",
+        short_name: "!see",
+        description: "bang!",
+        theme_color: "#0c0a09",
+        icons: [
+          {
+            src: "search.svg",
+            type: "image/svg+xml",
+          },
+        ],
+      },
+    }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "_headers",
+          dest: "",
+        },
+      ],
     }),
   ],
 });
